5f1afe6012c73c5a0e5ffbf511e6796a0980cdf0
Valleryan Virgil Zuliuskandar <valleryan1212@gmail.com>
2023-06-21 10:28:14 +0700

    Minor Fix

--- resources/views/landing/layouts/main.blade.php
+++ resources/views/landing/layouts/main.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-Learning Bank Bengkulu</title>
+    <link rel="icon" href="{{ asset('storage/images/logo_small.png') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="{{ asset('storage/images/logo_smalls.png') }}" />
+
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
